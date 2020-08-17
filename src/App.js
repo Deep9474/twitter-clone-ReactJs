14dfd37d41bb6ab09f0f3701de3cce7f62753789
Deep KAsundra <deepkasundra17@gmail.com>
2020-08-17 13:22:27 +0530

    sidebar

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Sidebar from './Sidebar';
+
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Twitter</h1>
+     <Sidebar />
+    </div>
+  );
+}
+
+export default App;
